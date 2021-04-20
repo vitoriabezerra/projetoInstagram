da4653e735e27b3df5015a97091b2ab2131a6c21
Vitória Bezerra <vitoria0406@gmail.com>
2021-04-20 12:01:13 -0300

    git commit -m

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -4,7 +4,7 @@ const usuariosController = {
     index: async (request, response) => {
         let usuarios =  await Usuario.findAll();
         
-        return response.json(usuarios);
+        return response.render('usuarios', { listaUsuarios: usuarios});
     },
     create: async (request, response) => {
         let {nome, email, senha} = request.body;
